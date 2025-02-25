de1ebaa18b266bf4973a5da53fc9d0941791772a
Marcus Gadelha <gadelhamarcus27@gmail.com>
2025-02-25 20:01:52 -0300

    employee details

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- R$ 10', 'Eggs Benedict - R$20', 'Oatmeal-- R$5', 'Frittata--R$30'];
+const mainCourseMenu = ['Steak --R$10', 'Pasta--R$15', 'Burger--R$20', 'Salmon--R$10'];
+const dessertMenu = ['Cake--R$30', 'Ice Cream--R$12', 'Pudding--R$30', 'Fruit Salad--R$10'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
