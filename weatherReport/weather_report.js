8267da5823db6376e14009b2eafc897bdf40be4d
marcusAires <gadelhamarcus27@gmail.com>
2025-03-10 20:00:56 -0300

    mudanças no código do app de previsão do tempo, ajustei a API para retornar resultados no meu idioma e corriji elementos HTML para portugues, também corrigi a forma que o JS recebe o input cidade para que ele converta em lowercase e gere um resultado (quando a cidade era escrita com letra maiuscula não era encontrada

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,17 +1,17 @@
 function showweatherDetails(e) {
     e.preventDefault();
-    const city = document.getElementById('city').value;
+    const city = document.getElementById('city').value.toLowerCase();
     const apiKey = '1bdb4beca58ddd8df4dea232646f30fe'; // Replace 'YOUR_API_KEY' with your actual API key
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;    
-    
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`;
+
     fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        const weatherInfo = document.getElementById('weatherInfo');
-        weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
-        <p>Temperature: ${data.main.temp} &#8451;</p>
-        <p>Weather: ${data.weather[0].description}</p>`;
-    })
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<h2>Clima em ${data.name}</h2>
+        <p>Temperatura: ${data.main.temp} &#8451;</p>
+        <p>Clima: ${data.weather[0].description}</p>`;
+        })
 }
 
-document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+document.getElementById('weatherForm').addEventListener('submit', showweatherDetails);
